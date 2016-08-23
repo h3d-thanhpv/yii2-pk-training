73e2b06a283f0196368ef0666ea27bb3ff2eebfd
Phan Van Thanh <thanhpv@fimo.edu.vn>
2016-08-23 16:25:25 +0700

    Add view angular

--- views/site/angular.php
+++ views/site/angular.php
@@ -2,12 +2,15 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use app\assets\JsAsset;
 
 
 /* @var $this yii\web\View */
 
 $this->title = Yii::t('app', 'Angular demo');
 $this->params['breadcrumbs'][] = $this->title;
+
+JsAsset::register($this);
 ?>
 
 <div class="site-angular" ng-app="pkTrainApp" ng-controller="loadBookCtrl">
@@ -15,6 +18,13 @@ $this->params['breadcrumbs'][] = $this->title;
         <label for="exampleInputEmail1">Book Id</label>
         <input type="text" class="form-control" id="bookInputId" placeholder="Book id" ng-model="bookId">
     </div>
-</div>
 
-<?php $this->registerJsFile('@web/js/demo.js', ['depends' => [\app\assets\AngularAsset::className()]]) ?>
+    <p>ID: <span ng-bind="bookInfo.id"></span></p>
+    <p>code: <span ng-bind="bookInfo.code"></span></p>
+    <p>name: <span ng-bind="bookInfo.name"></span></p>
+    <p>description: <span ng-bind="bookInfo.description"></span></p>
+    <p>author: <span ng-bind="bookInfo.author"></span></p>
+    <p>category: <span ng-bind="bookInfo.category"></span></p>
+    <p>publishing_year: <span ng-bind="bookInfo.publishing_year"></span></p>
+    <p>publishing_company: <span ng-bind="bookInfo.publishing_company"></span></p>
+</div>
