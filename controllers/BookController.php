c01b5832b7ac0ae4673d6b744e915186242674d2
Phan Van Thanh <thanhpv@fimo.edu.vn>
2016-08-23 10:57:51 +0700

    Generate CRUD for book table

---
+++ controllers/BookController.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace app\controllers;
+
+class BookController extends base\BookController
+{
+
+}
---
+++ views/book/_search.php
@@ -0,0 +1,41 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\BookSearch */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="book-search">
+
+    <?php $form = ActiveForm::begin([
+        'action' => ['index'],
+        'method' => 'get',
+    ]); ?>
+
+    <?= $form->field($model, 'id') ?>
+
+    <?= $form->field($model, 'code') ?>
+
+    <?= $form->field($model, 'name') ?>
+
+    <?= $form->field($model, 'description') ?>
+
+    <?= $form->field($model, 'author') ?>
+
+    <?php // echo $form->field($model, 'category') ?>
+
+    <?php // echo $form->field($model, 'publishing_year') ?>
+
+    <?php // echo $form->field($model, 'publishing_company') ?>
+
+    <div class="form-group">
+        <?= Html::submitButton(Yii::t('app', 'Search'), ['class' => 'btn btn-primary']) ?>
+        <?= Html::resetButton(Yii::t('app', 'Reset'), ['class' => 'btn btn-default']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
