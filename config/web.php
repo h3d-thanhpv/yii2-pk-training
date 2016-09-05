06946eb19ec2f61ebe717a919b9fab4be4252d5f
Phan Van Thanh <thanhpv@fimo.edu.vn>
2016-09-05 22:53:12 +0700

    Add i18n component :sunglasses:

--- config/web.php
+++ config/web.php
@@ -6,6 +6,7 @@ $config = [
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
+//    'language' => 'jp-JP',
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
@@ -44,6 +45,20 @@ $config = [
             'rules' => [
                 '<controller>/<action:(view)>/<id:\d+>' => '<controller>/<action>',
                 '<controller>s' => '<controller>/index',
+            ],
+        ],
+        'i18n' => [
+            'translations' => [
+                'app*' => [
+                    'class' => 'yii\i18n\PhpMessageSource',
+                    'basePath' => '@app/messages',
+                    'sourceLanguage' => 'en-US',
+                    'fileMap' => [
+                        'app' => 'app.php',
+                        'app/error' => 'error.php',
+                        'app/pk' => 'pk.php'
+                    ],
+                ],
             ],
         ],
     ],
