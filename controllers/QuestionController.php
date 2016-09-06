ce9702fdf7fba73b2ae1a809b208cdf01dc8f548
Phan Van Thanh <thanhpv@fimo.edu.vn>
2016-09-06 14:59:31 +0700

    [Demo] [Create Question] Create model and controller :cupid:

---
+++ controllers/QuestionController.php
@@ -0,0 +1,36 @@
+<?php
+namespace app\controllers;
+
+
+use app\models\Question;
+use yii\filters\VerbFilter;
+
+class QuestionController extends base\QuestionController
+{
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return array_merge(parent::behaviors(), [
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'create-question' => ['POST'],
+                ],
+            ],
+        ]);
+    }
+
+    public function actionNewQuestion()
+    {
+        return $this->render('new');
+    }
+
+    public function actionCreateQuestion()
+    {
+        $model = new Question();
+        $model->content = \Yii::$app->request->getRawBody();
+        $model->save();
+    }
+}
