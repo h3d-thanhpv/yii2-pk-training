d1f77abc5b79b34973c88f4b8637d9a10f6604bd
Phan Van Thanh <thanhpv@fimo.edu.vn>
2016-09-06 14:58:37 +0700

    [Demo] [Create question] Gii generator for model, controller and views

---
+++ models/base/Question.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace app\models\base;
+
+use Yii;
+
+/**
+ * This is the model class for table "{{%question}}".
+ *
+ * @property integer $id
+ * @property string $content
+ */
+class Question extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return '{{%question}}';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['content'], 'string'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('app', 'ID'),
+            'content' => Yii::t('app', 'Content'),
+        ];
+    }
+}
