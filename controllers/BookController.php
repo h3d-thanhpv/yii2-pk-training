92d35c28468f9b64b999777effc6f48eaf1e947e
Phan Van Thanh <thanhpv@fimo.edu.vn>
2016-09-08 14:56:13 +0700

    Demo for tag 0.9 :sweat_drops:

--- controllers/BookController.php
+++ controllers/BookController.php
@@ -2,6 +2,7 @@
 
 namespace app\controllers;
 
+use app\models\Book;
 use yii\filters\AccessControl;
 
 class BookController extends base\BookController
@@ -24,5 +25,14 @@ class BookController extends base\BookController
                 ],
             ],
         ]);
+    }
+
+    public function actionViewAllBook()
+    {
+        $id = 1;
+        /** @var Book[] $books */
+        $books = Book::find()->select(['code', 'name', 'author as tacgia'])->where(['id' => $id])->all();
+        echo $books[0]->totalBook;
+        var_dump($books);
     }
 }
--- models/Book.php
+++ models/Book.php
@@ -2,8 +2,21 @@
 
 namespace app\models;
 
-
+/**
+ * @property string $totalBook
+ * @property string $quality
+ */
 class Book extends base\Book
 {
+    private $_quality = 1000;
 
+    public function getQuality()
+    {
+        return $this->_quality;
+    }
+
+    public function getTotalBook()
+    {
+        return self::find()->count();
+    }
 }
