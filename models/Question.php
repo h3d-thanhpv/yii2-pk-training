cf9cacbf7f7b82c956cf28fa56c36f23baf02851
Phan Van Thanh <thanhpv@fimo.edu.vn>
2016-09-15 11:42:16 +0700

    create getter for totalQuestion :rose:

--- models/Question.php
+++ models/Question.php
@@ -1,8 +1,14 @@
 <?php
 namespace app\models;
 
-
+/**
+ * @property integer $totalQuestion
+ */
 class Question extends base\Question
 {
+    public function getTotalQuestion()
+    {
+        return self::find()->count();
+    }
 
 }
