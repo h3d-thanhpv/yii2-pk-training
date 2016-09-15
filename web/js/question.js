69831716e730804b3d2ed88e2755c43c46b771dc
Phan Van Thanh <thanhpv@fimo.edu.vn>
2016-09-15 11:17:58 +0700

    add library upload and modify js file :oncoming_automobile:

--- web/js/question.js
+++ web/js/question.js
@@ -1,12 +1,13 @@
-var projectApp = angular.module('questionApp', []);
+var projectApp = angular.module('questionApp', ['ngFileUpload']);
 
-projectApp.controller('newController', ['$scope', '$http', '$filter', '$q', '$window', function($scope, $http, $filter, $q, $window) {
+projectApp.controller('newController', ['$scope', '$http', '$filter', '$q', '$window', 'Upload', function($scope, $http, $filter, $q, $window, Upload) {
     $scope.questionInfo = {
         "question":"",
         "answers":[{
             "content":"",
             "isTrue": true
-        }]
+        }],
+        "image":""
     };
     $scope.addAnswer = function () {
         $scope.questionInfo.answers.push({
@@ -19,7 +20,30 @@ projectApp.controller('newController', ['$scope', '$http', '$filter', '$q', '$wi
         $http.post('create-question', $scope.questionInfo).success(function() {
             $window.location.href = 'index';
         });
-    }
+    };
+
+    // upload later on form submit or something similar
+    $scope.submit = function() {
+        if ($scope.form.file.$valid && $scope.file) {
+            $scope.upload($scope.file);
+        }
+    };
+
+    // upload on file select or drop
+    $scope.upload = function (file) {
+        Upload.upload({
+            url: 'upload-image',
+            data: {file: file, 'username': $scope.username}
+        }).then(function (resp) {
+            console.log('Success ' + resp.config.data.file.name + ' uploaded. Response: ' + resp.data);
+            $scope.questionInfo.image = resp.data;
+        }, function (resp) {
+            console.log('Error status: ' + resp.status);
+        }, function (evt) {
+            $scope.progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+            console.log('progress: ' + $scope.progressPercentage + '% ' + evt.config.data.file.name);
+        });
+    };
 }]).config(['$httpProvider', function ($httpProvider) {
     $httpProvider.defaults.headers.post['X-CSRF-Token'] = $('meta[name="csrf-token"]').attr("content");
     $httpProvider.defaults.headers.common['Accept'] = 'application/json, text/javascript';
