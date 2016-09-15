c74eb08eed43f4cce905cc073118139e859784b0
Phan Van Thanh <thanhpv@fimo.edu.vn>
2016-09-15 11:14:32 +0700

    Add question asset :dizzy:

--- assets/QuestionAsset.php
+++ assets/QuestionAsset.php
@@ -3,17 +3,20 @@
 namespace app\assets;
 
 
+use yii\bootstrap\BootstrapPluginAsset;
 use yii\web\AssetBundle;
 
 class QuestionAsset extends AssetBundle
 {
     public $sourcePath = '@webroot/js';
     public $js = [
+        'ng-file-upload-all.min.js',
         'question.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
         'yii\bootstrap\BootstrapAsset',
+        'yii\bootstrap\BootstrapPluginAsset',
         'app\assets\AngularAsset',
     ];
 }
